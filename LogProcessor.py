251c46a99557a75f2d7772067780b12d5f4a302c
Sara Farazi <s.farazi90@gmail.com>
2017-06-08 01:20:06 -0600

    database sample is created

--- LogProcessor.py
+++ LogProcessor.py
@@ -4,23 +4,33 @@ import sqlite3
 
 directory = 'data/'
 
+punctuations = ["'", '"', ":", ";", "-", "_", ")", "(", "[", "]", "{", "}"]
+
 conn = sqlite3.connect("data/query.db")
 curs = conn.cursor()
-# curs.execute("create table qlog(id integer, query text, time text)")
-# conn.commit()
+curs.execute("create table qlog(id integer, query text, time text)")
+conn.commit()
 
 for filename in os.listdir(directory):
-	if filename.endswith(".txt"):
-		with open(directory + filename) as f:
-			for line in f:
-				parts = line.split("\t")
-				entry = {
-					"id": parts[0],
-					"query": parts[1],
-					"time": parts[2],
-				}
-				quer = "insert into qlog values('{}','{}','{}')".format(parts[0], parts[1], parts[2])
-				pdb.set_trace()
-				curs.execute(quer)
+    if filename.endswith(".txt"):
+        with open(directory + filename) as f:
+            for line in f:
+                parts = line.split("\t")
+                entry = {
+                    "id": parts[0],
+                    "query": parts[1],
+                    "time": parts[2],
+                }
+
+                
+                for item in punctuations:
+                    if item in parts[1]:
+                        parts[1] = parts[1].replace(item, "")
+
+
+
+                quer = "insert into qlog values('{}','{}','{}')".format(parts[0], parts[1], parts[2])   
+                # print(quer)
+                curs.execute(quer)
 
 conn.commit()
