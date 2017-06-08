06c70d797dc4a67ee2b365a9a1d40401961bc330
Sara Farazi <s.farazi90@gmail.com>
2017-06-08 01:06:26 -0600

    queries are saved in database

---
+++ LogProcessor.py
@@ -0,0 +1,26 @@
+import os
+import pdb
+import sqlite3
+
+directory = 'data/'
+
+conn = sqlite3.connect("data/query.db")
+curs = conn.cursor()
+# curs.execute("create table qlog(id integer, query text, time text)")
+# conn.commit()
+
+for filename in os.listdir(directory):
+	if filename.endswith(".txt"):
+		with open(directory + filename) as f:
+			for line in f:
+				parts = line.split("\t")
+				entry = {
+					"id": parts[0],
+					"query": parts[1],
+					"time": parts[2],
+				}
+				quer = "insert into qlog values('{}','{}','{}')".format(parts[0], parts[1], parts[2])
+				pdb.set_trace()
+				curs.execute(quer)
+
+conn.commit()
